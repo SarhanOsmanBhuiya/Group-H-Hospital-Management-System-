ae00475702c1a78fa4e4bbba83e692dba48d326f
sameerhashmi36 <43946472+sameerhashmi36@users.noreply.github.com>
2018-12-11 13:42:23 +0600

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php include('part/db_connect.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CONTACT</title>
+</head>
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+<body>
+
+	<div class="wrap container">
+		
+		<div class="navbar">
+			<div class="row">
+				<ul>
+					<li><a href="home.php">HOME</a></li>
+					<li><a href="pa.php">PATIENTS</a></li>
+					<li><a href="ward.php">WARDS</a></li>
+					<li><a href="bed.php">BED</a></li>
+					<li><a href="nurse.php">NURSE</a></li>
+					<li><a href="medcine.php">MEDICINE</a></li>
+					<li><a href="pescription.php">PESCRIPTION</a></li>
+					<li><a href="doctor.php">DOCTORS</a></li>
+					<li><a href="information.php">INFORMATION</a></li>
+					<li><a href="contact.php">CONTACT</a></li>
+				</ul>
+			</div>
+		</div>
+    
+ 
+</body>
+</html>
+
+<?php include('part/db_close.php'); ?>
