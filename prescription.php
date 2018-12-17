d25c51721c98b564fd2d851f7a5013d61a0e09bc
sameerhashmi36 <43946472+sameerhashmi36@users.noreply.github.com>
2018-12-17 12:10:11 +0600

    Update and rename prescription.html to prescription.php

---
+++ prescription.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prescription</title>
+</head>
+	
+
+<body>
+
+	<div class="wrap container">
+		
+		<div class="navbar">
+			<div class="row">
+				<ul>
+					<li><a href="home.php">HOME</a></li>
+					<li><a href="pa.php">PATIENTS</a></li>
+					<li><a href="ward.php">WARDS</a></li>
+					<li><a href="bed.php">BED</a></li>
+					<li><a href="nurse.php">NURSE</a></li>
+					<li><a href="medcine.php">MEDICINE</a></li>
+					<li><a href="pescription.php">PESCRIPTION</a></li>
+					<li><a href="doctor.php">DOCTORS</a></li>
+					<li><a href="information.php">INFORMATION</a></li>
+					<li><a href="contact.php">CONTACT</a></li>
+				</ul>
+			</div>
+		</div>
+</body>
